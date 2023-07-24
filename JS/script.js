6fbf2fc3f3de01650f03d3e666f7777ed92d7803
muqniansyah <muqniansyah@gmail.com>
2023-07-24 17:32:12 +0700

    fix bug

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,9 @@
 //Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika hamburger menu diklik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // Toggle class active untuk search form
@@ -21,7 +22,7 @@ document.querySelector("#search-button").onclick = (e) => {
 // Toogle class active untuk shopping-cart
 const shoppingCart = document.querySelector(".shopping-cart");
 // ketika shopping-cart diklik
-document.querySelector("#shopping-cart").onclick = (e) => {
+document.querySelector("#shopping-cart-button").onclick = (e) => {
   shoppingCart.classList.toggle("active");
   // suapaya aksi defaultnya tidak dilakukan
   // membajak tag a/link tidak jalan agar saat diklik dimanapun tidak scroll keatas.
@@ -29,9 +30,10 @@ document.querySelector("#shopping-cart").onclick = (e) => {
 };
 
 // klik diluar elemen
+// document.querySelector berfungsi mencari elemen yang pertama kali ditemukan saja
 const hamburger = document.querySelector("#hamburger-menu");
-const searchButton = documnet.querySelector("#search-button");
-const shoppingCartButton = document.querySelector("#shopping-cart");
+const searchButton = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
   //jika di klik diluar hamburger menu dan navbar atau selama yg diklik bukan navbar dan hamburger menu berarti kita hilangin class aktifnya.
@@ -46,21 +48,37 @@ document.addEventListener("click", function (e) {
   }
 
   // jika diklik diluar shopping-cart
-  if (
-    !shoppingCartButton.contains(e.target) &&
-    !shoppingCart.contains(e.target)
-  ) {
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
     shoppingCart.classList.remove("active");
   }
 });
 
 // Modal Box
-const itemDetailModal = documnet.querySelector("#item-detail-modal");
-const itemDetailButton = document.querySelector(".item-detail-button");
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
 
-// ketika tombol diklik
-itemDetailButton.onclick = (e) => {
-  // kita ambil modalnya dan kita ubah style display jadi flex
-  itemDetailModal.style.display = "flex";
+// looping semua elemen dari itemDetailButton
+itemDetailButton.forEach((btn) => {
+  // ketika tombol diklik
+  btn.onclick = (e) => {
+    // kita ambil modalnya dan kita ubah style display jadi flex
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+// cari elemen close icon yang ada didalam modal dan ketika diklik
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
   e.preventDefault();
+};
+
+// klik diluar modal untuk tutup close
+window.onclick = (e) => {
+  // jika yang ditarget adalah modal, maka
+  if (e.target === itemDetailModal) {
+    // hilangkan modalnya
+    itemDetailModal.style.display = "none";
+  }
 };
